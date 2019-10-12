2373629e9589ded468e7e7136917a8d3f05ee3c1
oboking <boking@student.chalmers.se>
2019-10-13 00:59:57 +0200

    adds posters for movies

--- flavium-dashboard/src/components/DownloadList/Item/index.js
+++ flavium-dashboard/src/components/DownloadList/Item/index.js
@@ -1,20 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import { ItemStyle, Name, Percentage, Bottom, ProgressBar, Size } from "./styles"
-
+import { ItemStyle, Name, Percentage, Bottom, ProgressBar} from "./styles"
+require('dotenv').config()
 const parseTorrent = require('parse-torrent')
 
 const Item = ({showList, magnetLink})  => {
 
-    useEffect(()=> {
-        getTorrentInfo(magnetLink)
-    }, [])
+  useEffect(()=> {
+    getTorrentInfo(magnetLink)
+  }, [])
 
   const [name, setName] = useState("It: Chapter 2");
+  const [posterSrc, setPosterSrc] = useState("");
 
   let size = 700.0;
   let doneSize = 254.2;
   let status = "Downloading";
-  let started = "2019-10-10";
+
+  const getPoster = async(url) => {
+    const api_call = await fetch(url);
+    const data = await api_call.json();
+    setPosterSrc("http://image.tmdb.org/t/p/w200//" +data.results[0].poster_path);
+    }
 
   const getTorrentInfo = (link) => {
     const torrentData = parseTorrent(link)
@@ -22,11 +28,18 @@ const Item = ({showList, magnetLink})  => {
     let stringVal = torrentData.name
     stringVal = stringVal.replace(/\./g,' ')
     stringVal = stringVal.match(regex)[0];
+    stringVal = stringVal.substring(0, stringVal.length - 1);
+    var lastIndex = stringVal.lastIndexOf(" ");
+    
+    stringVal = stringVal.substring(0, lastIndex);
+
+    const posterQuery = "https://api.themoviedb.org/3/search/movie?api_key="+ process.env.REACT_APP_MOVIE_KEY +"&query="+(stringVal.replace(/ /g,"%20"));
+    getPoster(posterQuery)
     setName(stringVal)
   }
 
   return (
-      <ItemStyle showList={showList}>
+      <ItemStyle showList={showList} posterSrc={posterSrc}>
           <Name showList={showList}>{name}</Name>
           {/* <Size>{size.toFixed(1)}MB</Size>*/}
           <Bottom showList={showList}>
