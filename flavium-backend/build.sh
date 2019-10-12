75eb44d14b3c57ef4930215cfaa84e209ada9c0b
vilddjur <oskar.jedvert@gmail.com>
2019-10-13 00:36:28 +0200

    Add build deps update compose file

--- flavium-backend/build.sh
+++ flavium-backend/build.sh
@@ -1,1 +1,9 @@
-go build -v
+#!/bin/sh
+export PATH=$PATH:$GOPATH/bin
+sh -c 'protoc -I/usr/local/include -I. \
+  -I/root/go/src \
+  -I/root/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:. \
+  --grpc-gateway_out=logtostderr=true:. pkg/torrents/torrent.proto'
+
+go build -v pkg/cmd/main.go
