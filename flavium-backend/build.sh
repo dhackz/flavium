d185f79d995decd249d7f5fe3019a557f2f80deb
vass <albin.vass@gmail.com>
2019-10-13 00:56:56 +0200

    Build app with correct permissions and owner
    Fix makefiles

--- flavium-backend/build.sh
+++ flavium-backend/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export PATH=$PATH:$GOPATH/bin
 sh -c 'protoc -I/usr/local/include -I. \
-  -I/root/go/src \
-  -I/root/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  -I/tmp/go/src \
+  -I/tmp/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --go_out=plugins=grpc:. \
   --grpc-gateway_out=logtostderr=true:. pkg/torrents/torrent.proto'
 
