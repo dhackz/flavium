4921059c68bc3215ad47da93a03f0e1ae314f655
oboking <boking@student.chalmers.se>
2019-10-19 01:07:27 +0200

    adds more details the detailed view

--- flavium-dashboard/src/components/DownloadList/Item/Details/index.js
+++ flavium-dashboard/src/components/DownloadList/Item/Details/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {DetailsStyle, Title, Grid, Description, Trailer} from "./styles"
+import {DetailsStyle, Title, Grid, Description, Trailer, DescriptionStyle, Score, HorizontalDiv, Genres, Runtime, Budget, Revenue} from "./styles"
 
 import ReactPlayer from 'react-player'
 
-const Details = ({name, description, youtubeId, voteAverage}) => {
+const Details = ({name, description, youtubeId, voteAverage, genres, releaseDate, tagline, budget, revenue, runtime}) => {
 
     const url = "https://www.youtube.com/watch?v=" + youtubeId
     return (
@@ -14,8 +14,16 @@ const Details = ({name, description, youtubeId, voteAverage}) => {
                 </Trailer>
                 <div>
                     <Description>
-                    <Title>{name}</Title>
-                        {description}
+                        <HorizontalDiv>
+                            <Title><div>{name} ({releaseDate})</div></Title>
+                            <Score>{"IMDb: " + voteAverage}</Score>
+                        </HorizontalDiv>
+                        <HorizontalDiv>
+                            <Genres>{genres.map((genre) => (genre + " "))}</Genres>
+                            <Runtime>{runtime} min</Runtime>
+                        </HorizontalDiv>
+                            <div>Result: ${(revenue-budget).toLocaleString()} <Revenue>(+${revenue.toLocaleString()})</Revenue>/<Budget>(-${budget.toLocaleString()})</Budget></div>
+                        <DescriptionStyle>{description}</DescriptionStyle>
                     </Description>
                 </div>  
             </Grid>
