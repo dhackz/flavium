02dc570c0c64fbc81d9db4e7053fd64393f5331d
oboking <boking@student.chalmers.se>
2019-10-19 01:09:29 +0200

    adds conditional rendering to currentdownloads if undefined

--- flavium-dashboard/src/components/DownloadList/index.js
+++ flavium-dashboard/src/components/DownloadList/index.js
@@ -40,7 +40,7 @@ const DownloadList = ({postListener}) => {
         <Toggle setShowList={setShowList} showList={showList}/>
       </Header>
       {itemColumns}
-      <ListStyle showList={showList}>
+      {currentDownloads && <ListStyle showList={showList}>
           {currentDownloads.map((item,key) => {
               let isExpanded = false;
               if(isListExpanded && key===indexOfExpanded){
@@ -58,7 +58,7 @@ const DownloadList = ({postListener}) => {
                 />
               );
             })}
-      </ListStyle>
+      </ListStyle>}
     </div>
   )
 };
