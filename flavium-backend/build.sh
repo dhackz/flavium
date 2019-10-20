137d2f2cc851d24fb2b75ceee91aa2369675c714
vass <albin.vass@gmail.com>
2019-10-20 16:05:31 +0200

    Separate build job and unittest job

--- flavium-backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:$(go env GOPATH)/bin
-protoc -I/usr/local/include -I. \
-    -I$(go env GOPATH)/src \
-    -I$(go env GOPATH)/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=plugins=grpc:. \
-  --grpc-gateway_out=logtostderr=true:. pkg/torrents/torrent.proto
-
-go build -v pkg/cmd/main.go
